f2223345bd61717607240a178a90bc7488071f8c
Mani3010 <106655094+Mani3010@users.noreply.github.com>
2024-06-27 17:36:12 +0530

    Add files via upload

---
+++ assignment_module47/assignment.js
@@ -0,0 +1,44 @@
+/*Q1
+console.log(addNumbers(3, 5)); 
+function addNumbers(a, b) {
+  return a + b;
+}
+  */
+/* Q2
+console.log(multiplyNumbers(3, 5));
+function multiplyNumbers(a, b) {
+    return a * b;
+  };
+  */
+ /* Q3
+  function sumNumbers(a, b) {
+    console.log(myVar); //variable hoisting
+    var myVar = a + b;
+    return myVar;
+  }
+  console.log(sumNumbers(3, 5)); 
+  */
+ /*Q4
+ {
+    var varVariable;
+    let letVariable;
+    const constVariable="This is a const variable";//const must be initialized
+    console.log(varVariable);
+    console.log(letVariable);
+    console.log(constVariable);
+    varVariable = "This is a var variable";
+   letVariable = "This is a let variable";
+    console.log(varVariable); // This will log the assigned value
+    console.log(letVariable); // This will log the assigned value
+    console.log(constVariable); // This will log the assigned value
+  }
+    */
+   /*Q5
+    let letVariable;
+    console.log(letVariable); 
+    letVariable = "This is a let variable";
+    console.log(letVariable);
+    */
+  
+  
+  
